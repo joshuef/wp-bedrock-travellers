80ff029ac40eb7a371e8885fbeb64f0db0728b24
Josh Wilson <josh.wilson@sociomantic.com>
2014-12-24 14:59:25 +0000

    all menus in the one

--- web/app/themes/travellers/templates/header.php
+++ web/app/themes/travellers/templates/header.php
@@ -6,15 +6,13 @@
 			wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
 		endif;
 		?>
-	</nav>  
-
-	<nav class="collapsed js-navbar--languages navbar js-navbar" role="navigation">
 		<?php
 		if (has_nav_menu('language_navigation')) :
 			wp_nav_menu(array('theme_location' => 'language_navigation', 'menu_class' => 'nav navbar--languages__ul'));
 		endif;
 		?>
-	</nav>
+	</nav>  
+
 
 	<nav class="collapsed js-navbar--contact navbar js-navbar" role="navigation">
 	<?php //maybe this one is a manual one using options ?>
@@ -31,15 +29,12 @@
 <header class="banner-container" role="banner">
 	<div class="container banner">
 
-		<div class="navbar-toggle nav__item js-navbar-toggle" data-toggle="collapse" data-target=".js-navbar--main">
+		<div class="navbar-toggle js-navbar-toggle" data-toggle="collapse" data-target=".js-navbar--main">
 			<span class="burger__bar"></span>
 			<span class="burger__bar"></span>
 			<span class="burger__bar"></span>
 		</div>
 		<a class="navbar-brand js-site-nav" href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-		<!-- <div class="navbar-toggle nav__item js-navbar-toggle" data-toggle="collapse" data-target=".js-n">
-			<a class="">Contact</a>
-		</div> -->
 	</div>
 
 </header>
