b5fda7d9212c967846837958511c44ede1c98b24
Josh Wilson <josh.wilson@sociomantic.com>
2014-12-26 13:12:05 +0000

    added child pages to homepage

--- web/app/themes/travellers/templates/homepage.php
+++ web/app/themes/travellers/templates/homepage.php
@@ -13,6 +13,39 @@ Template Name: Home Page
     <div class="copy">
         <h2><?php the_title(); ?></h2>
         <?php the_content(); ?>
+        
+        <div class="homepage__sections">
+            <?php
+            global $post;
+
+            $args = array(
+                'post_type'      => 'page',
+                'posts_per_page' => -1,
+                'post_parent'    => $post->ID,
+                'order'          => 'ASC',
+                'orderby'        => 'menu_order'
+             );
+
+
+            $sections = new WP_Query( $args );
+
+            if ( $sections->have_posts() ) : ?>
+
+                <?php while ( $sections->have_posts() ) : $sections->the_post(); ?>
+
+                    <div id="parent-<?php the_ID(); ?>" class="homepage__section">
+
+                        <h3><?php the_title(); ?></h1>
+
+                        <?php the_content(); ?>
+
+                    </div>
+
+                <?php endwhile; ?>
+
+            <?php endif; wp_reset_query(); ?>
+            
+        </div>
     </div>
     
 </div>
