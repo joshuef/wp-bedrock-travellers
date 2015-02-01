aa25fa5ced5f34d67e46dd320afccab9a84befa6
Josh Wilson <josh.wilson@sociomantic.com>
2015-02-01 20:19:39 +0100

    book now in there and arrows

--- web/app/themes/travellers/templates/footer.php
+++ web/app/themes/travellers/templates/footer.php
@@ -1,8 +1,6 @@
 <footer class="content-info  footer" role="contentinfo">
   <div class="container">
     <script type="text/javascript" defer="defer">
-        //grunt load css hing
-        (function(e){function t(t,n,r,o){"use strict";function a(){for(var e,n=0;u.length>n;n++)u[n].href&&u[n].href.indexOf(t)>-1&&(e=!0);e?i.media=r||"all":setTimeout(a)}var i=e.document.createElement("link"),c=n||e.document.getElementsByTagName("script")[0],u=e.document.styleSheets;return i.rel="stylesheet",i.href=t,i.media="only x",i.onload=o||function(){},c.parentNode.insertBefore(i,c),a(),i}var n=function(r,o){"use strict";if(r&&3===r.length){var a=e.navigator,i=e.Image,c=!(!document.createElementNS||!document.createElementNS("http://www.w3.org/2000/svg","svg").createSVGRect||!document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image","1.1")||e.opera&&-1===a.userAgent.indexOf("Chrome")||-1!==a.userAgent.indexOf("Series40")),u=new i;u.onerror=function(){n.method="png",t(r[2])},u.onload=function(){var e=1===u.width&&1===u.height,a=r[e&&c?0:e?1:2];n.method=e&&c?"svg":e?"datapng":"png",n.href=a,t(a,null,null,o)},u.src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==",document.documentElement.className+=" grunticon"}};n.loadCSS=t,e.grunticon=n})(this);
         
 
         var trv__setupSelect2s = function ( )
@@ -51,15 +49,6 @@
 
         };
             
-
-        
-            var theme = '/wp-content/'
-            if( window.location.hostname === 'localhost' )
-            {
-                theme = '/app/'
-            }
-            var iconFolder = '//'+window.location.host + theme +'themes/travellers/images/icons/';
-            grunticon([iconFolder+"icons.data.svg.css", iconFolder+"icons.data.png.css", iconFolder+"icons.fallback.png.css"]);
     </script>
     <?php // dynamic_sidebar('sidebar-footer'); ?>
   </div>
--- web/app/themes/travellers/templates/maxbooking__include.php
+++ web/app/themes/travellers/templates/maxbooking__include.php
@@ -9,55 +9,65 @@
         <li>
             <label class="booking__label" for"ad_tt">Arrival</label>
             <select class="booking__input" name="ad_tt" id="ad_tt">
-                              <option value="01">01</option>
-                              <option value="02">02</option>
-                              <option value="03">03</option>
-                              <option value="04">04</option>
-                              <option value="05">05</option>
-                              <option value="06">06</option>
-                              <option value="07">07</option>
-                              <option value="08">08</option>
-                              <option value="09">09</option>
-                              <option value="10">10</option>
-                              <option value="11">11</option>
-                              <option value="12">12</option>
-                              <option value="13">13</option>
-                              <option value="14">14</option>
-                              <option value="15">15</option>
-                              <option value="16">16</option>
-                              <option value="17">17</option>
-                              <option value="18">18</option>
-                              <option value="19">19</option>
-                              <option value="20">20</option>
-                              <option value="21">21</option>
-                              <option value="22">22</option>
-                              <option value="23">23</option>
-                              <option value="24">24</option>
-                              <option value="25">25</option>
-                              <option value="26">26</option>
-                              <option value="27">27</option>
-                              <option value="28" selected="selected">28</option>
-                              <option value="29">29</option>
-                              <option value="30">30</option>
-                              <option value="31">31</option>
-                          </select>
+                <option value="01">01</option>
+                <option value="02">02</option>
+                <option value="03">03</option>
+                <option value="04">04</option>
+                <option value="05">05</option>
+                <option value="06">06</option>
+                <option value="07">07</option>
+                <option value="08">08</option>
+                <option value="09">09</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+                <option value="13">13</option>
+                <option value="14">14</option>
+                <option value="15">15</option>
+                <option value="16">16</option>
+                <option value="17">17</option>
+                <option value="18">18</option>
+                <option value="19">19</option>
+                <option value="20">20</option>
+                <option value="21">21</option>
+                <option value="22">22</option>
+                <option value="23">23</option>
+                <option value="24">24</option>
+                <option value="25">25</option>
+                <option value="26">26</option>
+                <option value="27">27</option>
+                <option value="28" selected="selected">28</option>
+                <option value="29">29</option>
+                <option value="30">30</option>
+                <option value="31">31</option>
+            </select>
+            <div class="arrow_box--left">  
+            </div>
             <select class="booking__input" name="ad_mm_yyyy" id="ad_mm_yyyy">
               <option  value="01/2015">Jan 15</option><option value="02/2015">Feb 15</option><option value="03/2015">Mar 15</option><option value="04/2015">Apr 15</option><option value="05/2015">May 15</option><option value="06/2015">Jun 15</option><option value="07/2015">Jul 15</option><option value="08/2015">Aug 15</option><option value="09/2015">Sep 15</option><option value="10/2015">Oct 15</option><option value="11/2015">Nov 15</option><option value="12/2015">Dec 15</option>            </select>
+            <div class="arrow_box--left"></div>
         </li>
         <li>
             <label class="booking__label" for="lengthofstay">Nights</label>
             <select class="booking__input" name="lengthofstay" id="lengthofstay">
                 <option value="1">1</option><option value="2">2</option><option value="3" selected="selected">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option><option value="11">11</option>
             </select>
+            <div class="arrow_box--left"></div>
         </li>
         <li>
             <label class="booking__label"  for="persons">Guests</label>
             <select class="booking__input" name="persons" id="persons">
                 <option value="1">1</option><option value="2" selected="selected">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option>
             </select>
+            <div class="arrow_box--left"></div>
         </li>
-        <li>
-          <input class="booking__button" type="submit" name="submit_widget1" value="Book Now">
+        <li class="booking_submission-row">
+            <div class="arrow_box--right"></div>
+            <input class="booking__button" type="submit" name="submit_widget1" value="Book">
+            </input>
+            <div class="icon-now">
+            </div>
+            <div class="arrow_box--left"></div>
         </li>
     </ul>
     </form>
