2569920adf6db9962d2cc82f16a4b1f13bc33897
Josh Wilson <joshuef@gmail.com>
2015-05-13 18:45:33 +0200

    some more styles

--- web/app/themes/travellers/templates/page-header.php
+++ web/app/themes/travellers/templates/page-header.php
@@ -1,4 +1,10 @@
 <div class="page-header">
+    <?php 
+    if (has_post_thumbnail()) 
+    {
+        the_post_thumbnail('thumbnail');
+    }
+     ?>
   <h1>
     <?php echo roots_title(); ?>
   </h1>
